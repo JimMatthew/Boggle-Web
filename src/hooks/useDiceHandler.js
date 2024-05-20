1b7a44d2e3d6de6f4371ae1f368b1e4a9526d9f3
james <28635265+JimMatthew@users.noreply.github.com>
2024-05-20 10:47:11 -0500

    fix

--- src/hooks/useDiceHandler.js
+++ src/hooks/useDiceHandler.js
@@ -1,49 +1,49 @@
-import { useGameContext } from '../context/GameProvider';
-import { dicePressedHandler } from '../utils/dicePressedHandler';
-//import { dicePressedHandler } from '../dicePressedHandler';
-const dph = dicePressedHandler();
+import { useGameContext } from '../context/GameProvider'
+import { dicePressedHandler } from '../utils/dicePressedHandler'
+
+const dph = dicePressedHandler()
 
 const useDiceHandler = () => {
-  const { state, dispatch } = useGameContext();
-  const { currWord, game } = state;
+  const { state, dispatch } = useGameContext()
+  const { currWord, game } = state
 
   const handleDieClick = (index) => {
-    if (game.isGameOver()) return;
+    if (game.isGameOver()) return
 
     if (dph.isPressed(index) !== -1) {
       if (dph.isPressed(index) >= 2 && dph.isLastPressed(index)) {
-        handleSubmit();
-        return;
-      }
-
-      const ix = dph.isPressed(index) + 1;
-      if (ix === 1 && dph.isLastPressed(index)) {
-        dispatch({ type: 'SET_CURR_WORD', payload: '' });
-        dph.clear();
-        dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() });
+        handleSubmit()
         return
       }
-      const newCurrWord = currWord.slice(0, ix);
-      dph.slicePressed(ix);
-      dispatch({ type: 'SET_CURR_WORD', payload: newCurrWord });
-      dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() });
+
+      const ix = dph.isPressed(index) + 1
+      if (ix === 1 && dph.isLastPressed(index)) {
+        dispatch({ type: 'SET_CURR_WORD', payload: '' })
+        dph.clear()
+        dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() })
+        return
+      }
+      const newCurrWord = currWord.slice(0, ix)
+      dph.slicePressed(ix)
+      dispatch({ type: 'SET_CURR_WORD', payload: newCurrWord })
+      dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() })
       return
     } else if (dph.isNextTo(index)) {
-      dph.press(index);
-      const newCurrWord = currWord + game.getDice()[index];
-      dispatch({ type: 'SET_CURR_WORD', payload: newCurrWord });
-      dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() });
+      dph.press(index)
+      const newCurrWord = currWord + game.getDice()[index]
+      dispatch({ type: 'SET_CURR_WORD', payload: newCurrWord })
+      dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() })
     }
-  };
+  }
 
   const handleSubmit = () => {
-    game.submitWord(currWord);
-    dispatch({ type: 'SET_CURR_WORD', payload: '' });
-    dph.clear();
-    dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() });
-  };
+    game.submitWord(currWord)
+    dispatch({ type: 'SET_CURR_WORD', payload: '' })
+    dph.clear()
+    dispatch({ type: 'SET_PRESSED', payload: dph.getPressed() })
+  }
 
-  return { handleDieClick, handleSubmit };
-};
+  return { handleDieClick, handleSubmit }
+}
 
-export default useDiceHandler;
+export default useDiceHandler
